d434b94ff5d600068ab412128f03156d9d287895
Razvan Popa <razvan.popa@softvision.ro>
2019-07-31 11:38:10 +0300

    Revert "11 - Maps"
    This reverts commit 801967af01d1672009c6d1743e9e827c3873bcd1.

--- app.js
+++ app.js
@@ -282,42 +282,10 @@ isFullAge6(20, 1990,1920,2000);
 // var ema = new SmithPerson('Emily', 1983, 'Diaz', 'hu');
 
 // ES6
-// function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'ro'){
-//     this.firstName = firstName;
-//     this.yearOfBirth = yearOfBirth;
-//     this.lastName = lastName;
-//     this.nationality = nationality;
-// }
-// var ion = new SmithPerson('ion', 1990);
-
-////////////////////////////////
-// Lecture:  Maps
-
-const question = new Map();
-question.set('question', 'What is the official name of the latest major JavaScript version?');
-question.set(1, 'ES5');
-question.set(2, 'ES6');
-question.set(3, 'ES2015');
-question.set(4, 'ES7');
-question.set('correct', 3);
-question.set(true, 'Correct answer');
-question.set(false, 'Wrong, try again');
-
-// console.log(question.get('question'));
-// console.log(question.size);
-
-// if(question.has(4)){
-//     question.delete(4);
-// }
-
-// question.forEach((value, key) => console.log(`This is ${key} and it's set to ${value}`));
-
-for (let [key, value] of question.entries()){   //.entries() return all entries of our map. 
-    //console.log(`This is ${key} and it's set to ${value}`)
-    if(typeof(key) === 'number'){
-        console.log(`Answer ${key}: ${value}`);
-    }
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'ro'){
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
 }
-const ans = parseInt(prompt('Write the corect answer'));
-
-console.log(question.get(ans === question.get('correct'))); // alternative to create an if else to check the answer. 
+var ion = new SmithPerson('ion', 1990);
